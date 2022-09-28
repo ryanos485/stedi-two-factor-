d94f6459559c5189e9fcfa7916fcd53fdcd6afac
Ryan Osborne <osb21006@byui.edum>
2022-09-28 14:59:20 -0600

    first commit

--- managecustomer.js
+++ managecustomer.js
@@ -84,7 +84,7 @@ function createcustomer(){
 
     $.ajax({
         type: 'POST',
-        url: '/customer',
+        url: 'https://dev.stedi.me/customer',
         data: JSON.stringify(customer),
         contentType: 'application/text',
         dataType: 'text',
@@ -96,7 +96,7 @@ function createcustomer(){
 
     $.ajax({
         type: 'POST',
-        url: '/user',
+        url: 'https://dev.stedi.me/user',
         data: JSON.stringify({'userName':email, email, password, phone, "birthDate":bday, 'verifyPassword':verifypassword}),//we are using the email as the user name
         success: function(data) { alert(data);
         window.location.href = "/index.html"},
